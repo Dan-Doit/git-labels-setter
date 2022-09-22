76f1102ccca620f728ea8c15a1422eb9c41490f9
jodan <spqjeks@naver.com>
2022-09-22 17:14:54 +0900

    feat(get, set): set method customize

---
+++ set.js
@@ -0,0 +1,28 @@
+// IMPORT LIB
+const githubLabelSync = require('github-label-sync');
+const labels = require('./labels.json')
+const dotenv = require('dotenv')
+const fs = require('fs');
+
+// SET : ENV VAR
+dotenv.config()
+const {TOKEN, ACCOUNT, REPOSITORY} = process.env
+
+// SET : GIT HUB INFO VAR
+githubLabelSync({
+    accessToken: TOKEN,
+    repo: `${ACCOUNT}/${REPOSITORY}`,
+    labels,
+    dryRun: false,
+}).then(labels => {
+
+    if (labels.length === 0) {
+        console.log('\x1b[33m%s\x1b[0m', 'Labels are already up to date');
+    } else {
+        labels.map(label => {
+            console.debug(`Label ===> ${label.name} updated`)
+        })
+        console.log('\x1b[32m%s\x1b[0m', 'SUCCESS ALL LABELS UPDATED');
+    }
+
+}).catch(e => console.error(e));
